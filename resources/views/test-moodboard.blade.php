dd8fa48d86cd9daadb77000c6ef9d706657ad3f6
sequent-hub <sequent@list.ru>
2025-08-26 19:13:37 +0300

    настройка action

--- resources/views/test-moodboard.blade.php
+++ resources/views/test-moodboard.blade.php
@@ -10,7 +10,6 @@
 </head>
 <body>
 <div class="container mx-auto py-8">
-    <h1>Holst и все работает!</h1>
     <!-- Только контейнер - все остальное делает пакет -->
     <div id="moodboard-container" style="height: 900px; width: 100%;" data-board-id="OMiJ8YdfdPY"></div>
 </div>
